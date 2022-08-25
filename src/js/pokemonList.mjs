ab58bbf55d2b6fc2f88b8d2b6a6a9a609717378f
SKOL web development <perezcatriel@gmail.com>
2022-08-25 15:49:26 -0300

    [FIX] arreglando pahts para produccion

--- src/js/pokemonList.mjs
+++ src/js/pokemonList.mjs
@@ -20,7 +20,7 @@ export function pokemonList(limit, offset) {
 
 const home = window.location;
 // console.log(home.href);
-if (home.href === "http://127.0.0.1:5500/index.html") {
+if (home.href === "http://127.0.0.1:5500/index.html" || home.href === "https://perezcatriel.github.io/poke-album/") {
 	pokemonList((limit = 10), (offset = 0));
 }
 
