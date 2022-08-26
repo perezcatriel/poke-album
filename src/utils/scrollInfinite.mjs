90345d2ccdc85eb1808c1826f0f391954354317b
SKOL web development <perezcatriel@gmail.com>
2022-08-26 11:14:02 -0300

    [FIX] arreglando pahts para produccion

--- src/utils/scrollInfinite.mjs
+++ src/utils/scrollInfinite.mjs
@@ -6,7 +6,7 @@ export function scrollInfinite(limit, offset) {
 	const home = "http://127.0.0.1:5500/index.html";
 
 	// cards.innerHTML = '';
-	if (home === window.location.href || home === 'https://perezcatriel.github.io/poke-album') {
+	if (home === window.location.href || home === 'https://perezcatriel.github.io/poke-album/') {
 		pokemonList(limit, offset);
 	}
 }
